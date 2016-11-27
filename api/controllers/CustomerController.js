6b548e292c522fb99119f1d2d854904bbe2d36e5
Suthaharan Tharmalingam <sutha@kurinchilion.com>
2016-11-26 19:02:33 -0500

    Edit customer

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -31,6 +31,23 @@ module.exports = {
 			if(err) return next(err);
 			res.view({ customers: customers });
 		});
+	},
+
+	edit: function(req, res, next){
+		Customer.findOne( req.param('id'), function foundCustomer(err, customer){
+			if(err) return next(err);
+			if(!customer) return next();
+			res.view({ customer: customer });
+		});
+	},
+
+	update: function(req, res, next){
+		Customer.update( req.param('id'), req.params.all(), function customerUpdated(err){
+			if(err) {
+				return res.redirect('/customer/edit/'+req.param('id')); 
+			}
+			res.redirect('/customer/show/' + req.param('id'));
+		});
 	}
 };
 
